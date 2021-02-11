5cbc8343c6c1be107464eb97e7190cc968437833
jasonschreib <jwschreiber1@gmail.com>
2021-02-10 21:05:28 -0800

    Created mongoDB schema

--- database/index.js
+++ database/index.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  nameOfUser: {
+      type: String,
+  required: true,
+    unique: true
+  },
+  repoId: {type: Number, required: true, unique: true},
+  repoName: {type: String, required: true, unique: true},
+  repoUrl: {type: String, required: true, unique: true},
+  forks_count: {type: Number, required: true},
+  watchers_count: {type: Number, required: true}
 });
+
+
 
 let Repo = mongoose.model('Repo', repoSchema);
 
