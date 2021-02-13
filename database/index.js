2d14c659bee2db61e90d4f18253ed61f83285638
jasonschreib <jwschreiber1@gmail.com>
2021-02-13 00:01:27 -0800

    Able to add entries to MongoDB

--- database/index.js
+++ database/index.js
@@ -7,29 +7,68 @@ let repoSchema = mongoose.Schema({
   nameOfUser: {
       type: String,
   required: true,
-    unique: true
+  unique: false
   },
-  repoId: {type: Number, required: true, unique: true},
-  repoName: {type: String, required: true, unique: true},
-  repoUrl: {type: String, required: true, unique: true},
-  forks_count: {type: Number, required: true},
-  watchers_count: {type: Number, required: true}
+  repoId: {type: Number, unique: true},
+  repoName: {type: String},
+  repoUrl: {type: String, unique: true},
+  forks_count: {type: Number},
+  watchers_count: {type: Number}
 });
 
 
-
 let Repo = mongoose.model('Repo', repoSchema);
+
 
 let save = (fetchData) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
-  console.log('Im in Mongo save function');
+  console.log('Im in Mongo save function', fetchData);
+  //iterate over the fetchData array
+  for (var i = 0; i < fetchData.length; i++) {
+    //create a new document from the model for each index in fetchData array
+    const doc = new Repo(
+      {
+        nameOfUser: fetchData[i].owner.login,
+       repoId: fetchData[i].id,
+       repoName: fetchData[i].name,
+       repoUrl: fetchData[i].html_url,
+       forks_count: fetchData[i].forks_count,
+       watchers_count: fetchData[i].watchers_count
+      }
+    );
+    doc.save((err, results) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(results);
+    })
+    //if an entry with the repo and username can be found
+      //update that entry with new info
+    //otherwise create a brand new entry
 
 
+    }
 
 
   //send response data back to the server
 }
 
 module.exports.save = save;
+
+
+    // Repo.findOne({ nameOfUser: fetchData[i].owner.login, repoId: fetchData[i].id })
+    //   .then((exists) => {
+    //     if (exists) {
+    //       console.log('FOUND AN ENTRY', i)
+    //       //otherwise entry not found
+    //       //call FindOneAndUpdate function to update this entry
+    //     } else {
+    //       console.log('DIDNT FIND ENTRY', i)
+    //       //add a new entry to database
+    //     }
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
